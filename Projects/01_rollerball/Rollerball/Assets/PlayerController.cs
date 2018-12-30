6f093373324877fb47e19cae1a6acd6c445a62e3
BanyaszJ <banyasz.joco@gmail.hu>
2018-12-30 20:54:54 +0100

    Gravity and physics update
    Added nice phics and gravity touches, so it feels less floaty.

--- Projects/01_rollerball/Rollerball/Assets/PlayerController.cs
+++ Projects/01_rollerball/Rollerball/Assets/PlayerController.cs
@@ -4,46 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 10;
-    private Rigidbody rb;
-    //    private 
+    // ------------Variables--------------
 
+    private Rigidbody rb;
+
+    public float Speed = 4.0f;
     public float moveHorizontal;
     public float moveVertical;
-    public float jumpHeight;
+    public float jumpHeight = 2.0f;
+
     public bool isGrounded;
+
+    //------------------------------------
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-    void Update()
-    {
-
-    }
     private void FixedUpdate()
     {
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetAxis("Vertical");
-        jumpHeight = 0f;
 
         if (isGrounded)
         {
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetButton("Jump"))
             {
-                jumpHeight = 100f;
+
+                rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+
+            }
+        }
+        else if (!isGrounded)
+        {
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += new Vector3(0, 1 * Physics.gravity.y * 5.0f * Time.deltaTime, 0);
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+
+                rb.velocity += new Vector3(0, 1 * Physics.gravity.y * 5.0f * Time.deltaTime, 0);
+
             }
         }
 
-        Vector3 Movement = new Vector3(moveHorizontal, jumpHeight, moveVertical);
+        Vector3 Movement = new Vector3(moveHorizontal * Speed, 0.0f, moveVertical * Speed);
         rb.AddForce(Movement * Speed);
-
 
     }
     private void OnGUI()
     {
-        string message = jumpHeight.ToString();
-        GUI.Label(new Rect(Screen.width / 5, Screen.height / 5, 200f, 200f), message);
+        //string message = jumpHeight.ToString();
+        string velocity = rb.velocity.ToString();
+        //GUI.Label(new Rect(Screen.width / 5, Screen.height / 5, 200f, 200f), message);
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 5, 200f, 200f), velocity);
     }
 
     void OnCollisionEnter(Collision other)
